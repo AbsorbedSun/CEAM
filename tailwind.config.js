f3ef82dabd3b995f01be7260e8990b676ba80ead
AbsorbedSun <aldoamb.garcia074@gmail.com>
2024-07-24 23:25:51 -0600

    Inicio

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/pages/*.html"],
+  content: ["./*.html", "./src/pages/**/*.html"],
   theme: {
     extend: {},
   },
